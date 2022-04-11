d78de1f6168ee05b90f2280a6203e32e79cc2905
Everton Ferens <evertonferens0@gmail.com>
2022-04-11 09:36:46 -0300

    Seller entity class

--- src/com/application/Main.java
+++ src/com/application/Main.java
@@ -1,8 +1,19 @@
 package com.application;
 
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Department obj = new Department(1, "Books");
+
+        Seller seller = new Seller (21,"BOB" ,"bob@gmail.com" ,new Date(),3000.0,obj);
+        System.out.println(seller);
+        // write your code here
     }
 }
